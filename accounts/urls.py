966b9b6da3a85dde0ede1814edc3ee7fa1326a6d
Yuta-Dohmae <yuta.doumae@goodsun-hd.com>
2024-05-31 13:54:02 +0900

    static

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = "accounts"
@@ -9,4 +10,4 @@ urlpatterns = [
     path('signup/', views.SignupView.as_view(), name="signup"),
     path('login/', views.LoginView.as_view(), name="login"),
     path('logout/', views.LogoutView, name="logout"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
